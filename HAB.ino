3cc6b347fc713cd2a48cdc4d7808a9790145b603
beamosec <beamosec@gmail.com>
2017-04-05 22:31:32 -0400

    fixed ttl camera functionality (v2)

--- HAB.ino
+++ HAB.ino
@@ -105,22 +105,22 @@ void setup() {
 // Loop to record data to a .csv file every 6 seconds. Program by Nick, Cedric, and Alex
 void loop() {
 
-if (! cam.takePicture())
-      Serial.println("Failed to snap!");
-    else
-      Serial.println("Picture taken!");
+  if (! cam.takePicture())
+    Serial.println("Failed to snap!");
+  else
+    Serial.println("Picture taken!");
 
-    // Create an image with the name IMAGExx.JPG
-    char filename[13];
-    strcpy(filename, "IMAGE00.JPG");
-    for (int i = 0; i < 500; i++) {
-      filename[5] = '0' + i / 10;
-      filename[6] = '0' + i % 10;
-      // create if does not exist, do not open existing, write, sync after write
-      if (! SD.exists(filename)) {
-        break;
-      }
+  // Create an image with the name IMAGExx.JPG
+  char filename[13];
+  strcpy(filename, "IMAGE00.JPG");
+  for (int i = 0; i < 100; i++) {
+    filename[5] = '0' + i / 10;
+    filename[6] = '0' + i % 10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
     }
+  }
 
   dataFile = SD.open("HawkData.csv", FILE_WRITE); //open the file to write to it
 
@@ -172,29 +172,6 @@ if (! cam.takePicture())
     Serial.println(" *f");
     Serial.println("");
 
-    uint16_t jpglen = cam.frameLength();
-    Serial.print("Storing ");
-    Serial.print(jpglen, DEC);
-    Serial.print(" byte image.");
-
-    int32_t time = millis();
-    pinMode(8, OUTPUT);
-    // Read all the data up to # bytes!
-    byte wCount = 0; // For counting # of writes
-    while (jpglen > 0) {
-      // read 32 bytes at a time;
-      uint8_t *buffer;
-      uint8_t bytesToRead = min(32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
-      buffer = cam.readPicture(bytesToRead);
-      dataFile.write(buffer, bytesToRead);
-      if (++wCount >= 64) { // Every 2K, give a little feedback so it doesn't appear locked up
-        Serial.print('.');
-        wCount = 0;
-      }
-      //Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
-      jpglen -= bytesToRead;
-    }
-
     Serial.println("");
 
     String dataString = String(event.acceleration.x) + ", " + String(event.acceleration.y) + ", " + String(event.acceleration.z);
@@ -214,6 +191,33 @@ if (! cam.takePicture())
     dataFile.print(heatindex);
 
     dataFile.close();
+
+File imgFile = SD.open(filename, FILE_WRITE);
+
+    // Get the size of the image (frame) taken
+    uint16_t jpglen = cam.frameLength();
+    Serial.print("Storing ");
+    Serial.print(jpglen, DEC);
+    Serial.print(" byte image.");
+
+    int32_t time = millis();
+    pinMode(8, OUTPUT);
+    // Read all the data up to # bytes!
+    byte wCount = 0; // For counting # of writes
+    while (jpglen > 0) {
+      // read 32 bytes at a time;
+      uint8_t *buffer;
+      uint8_t bytesToRead = min(32, jpglen); // change 32 to 64 for a speedup but may not work with all setups!
+      buffer = cam.readPicture(bytesToRead);
+      imgFile.write(buffer, bytesToRead);
+      if (++wCount >= 64) { // Every 2K, give a little feedback so it doesn't appear locked up
+        Serial.print('.');
+        wCount = 0;
+      }
+      //Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
+      jpglen -= bytesToRead;
+    }
+    imgFile.close();
 
   } else {
 
